a984c4b1917ec35ad919730b78e2a294c20690bf
mks1313 <mg.marinov@gmx.es>
2026-04-25 17:56:23 +0200

    chrore: hpp

--- CPP/CPP_06/ex00/inc/ScalarConverter.hpp
+++ CPP/CPP_06/ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 18:48:51 by mmarinov          #+#    #+#             */
-/*   Updated: 2026/04/16 21:26:49 by mmarinov         ###   ########.fr       */
+/*   Updated: 2026/04/25 14:58:18 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,30 @@
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
+typedef enum e_type
+{
+	TYPE_CHAR,
+	TYPE_INT,
+	TYPE_FLOAT,
+	TYPE_DOUBLE,
+	TYPE_SPECIAL,
+	TYPE_INVALID
+} t_type;
+
 class ScalarConverter
 {
 	private:
+		static t_type detectType(const std::string& literal);
 		static bool isPseudoLiteral(const std::string& literal);
 		static bool isCharLiteral(const std::string& literal);
 		static bool isIntLiteral(const std::string& literal);
 		static bool isFloatLiteral(const std::string& literal);
 		static bool isDoubleLiteral(const std::string& literal);
 
-		static void prinChar(double literal);
-		static void printInt(double literal);
-		static void printFloat(double literal);
-		static void printDouble(double literaal);
+		static void printChar(double value, t_type type);
+		static void printInt(double value, t_type type);
+		static void printFloat(double value, t_type type);
+		static void printDouble(double value, t_type type);
 
 	public:
 		static void convert(const std::string& literal);
